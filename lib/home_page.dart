d2024b3c698262818a666aeb78b9003d84d3a634
aorohith <aorohith@gmail.com>
2022-03-24 02:33:33 -0700

    Refactoring applied

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_designs/refactoring.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,58 +13,44 @@ class HomeScreen extends StatelessWidget {
         ),
         body: ListView(
           children: [
-            const ListTile(
-              leading: Icon(Icons.share),
-              title: Text(
-                "Share Dukaan App",
-                style: TextStyle(fontSize: 20),
-              ),
-              trailing: Icon(Icons.arrow_forward_ios),
+            AdditionalTiles(
+              leadIcon: Icons.share,
+              text: 'Share Dukan App',
+              trailIcon: Icon(Icons.arrow_forward_ios),
             ),
-            const ListTile(
-              leading: Icon(Icons.mark_chat_read_rounded),
-              title: Text(
-                "Change Language",
-                style: TextStyle(fontSize: 20),
-              ),
-              trailing: Icon(Icons.arrow_forward_ios),
+
+            AdditionalTiles(
+              leadIcon: Icons.mark_chat_read_rounded,
+              text: 'Change Language',
+              trailIcon: Icon(Icons.arrow_forward_ios),
             ),
-            ListTile(
-              leading: Icon(Icons.whatsapp),
-              title: Text(
-                "Whatsapp Chat Support",
-                style: TextStyle(fontSize: 20),
-              ),
-              trailing: Switch(
+            
+            AdditionalTiles(
+              leadIcon: Icons.whatsapp,
+              text: 'Whatsapp Chat Support',
+              trailIcon: Switch(
                 onChanged: editStatus(),
                 value: true,
                 inactiveThumbColor: Colors.blue,
                 inactiveTrackColor: Colors.blue[300],
               ),
             ),
-            ListTile(
-              leading: Icon(Icons.lock),
-              title: Text(
-                "Privacy Policy",
-                style: TextStyle(fontSize: 20),
-              ),
+
+            AdditionalTiles(
+              leadIcon: Icons.lock,
+              text: 'Privacy Policy',
             ),
 
-            ListTile(
-              leading: Icon(Icons.star_border_outlined),
-              title: Text(
-                "Rate Us",
-                style: TextStyle(fontSize: 20),
-              ),
+            AdditionalTiles(
+              leadIcon: Icons.star_border_outlined,
+              text: 'Rate Us',
             ),
 
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text(
-                "Log Out",
-                style: TextStyle(fontSize: 20),
-              ),
+            AdditionalTiles(
+              leadIcon: Icons.logout,
+              text: 'Sign Out',
             ),
+            
           ],
         ));
   }
---
+++ lib/refactoring.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AdditionalTiles extends StatelessWidget {
+
+  final leadIcon;
+  final text;
+  final trailIcon;
+
+   AdditionalTiles({ Key? key, required this.leadIcon, required this.text, this.trailIcon }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+              leading: Icon(leadIcon),
+              title: Text(
+                text,
+                style: const TextStyle(fontSize: 20),
+              ),
+              trailing: trailIcon,
+            );
+  }
+}
