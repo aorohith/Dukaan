8a1d311cfb71472ca7b475855d5654af51b0f329
aorohith <aorohith@gmail.com>
2022-03-25 05:56:28 +0530

    Dukaan Home Manage store finished completely

--- lib/dukan_home.dart
+++ lib/dukan_home.dart
@@ -23,16 +23,46 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Container(
           child: GridView(
               children: [
-                CardStore(boxIcon: Icons.campaign, text1: "Marketing", text2: "Designing",boxColor: Color(0xffF16E18),),
-                CardStore(boxIcon: Icons.currency_rupee, text1: "Marketing", text2: "Designing",boxColor: Color(0xff63D461)),
-                CardStore(boxIcon: Icons.ac_unit, text1: "Marketing", text2: "Designing",boxColor: Color(0xffF6B152)),
-                CardStore(boxIcon: Icons.switch_account_rounded, text1: "Marketing", text2: "Designing",boxColor: Color(0xff13AD9E)),
-                CardStore(boxIcon: Icons.qr_code, text1: "Marketing", text2: "Designing",boxColor: Color(0xff7D7D7D)),
-                CardStore(boxIcon: Icons.bookmarks_sharp, text1: "Marketing", text2: "Designing",boxColor: Color(0xff6E41B1)),
-                CardStore(boxIcon: Icons.format_align_left, text1: "Marketing", text2: "Designing",boxColor: Color(0xffC15A85)),
-                
+                CardStore(
+                  boxIcon: Icons.campaign,
+                  text1: "Marketing",
+                  text2: "Designing",
+                  boxColor: Color(0xffF16E18),
+                ),
+                CardStore(
+                    boxIcon: Icons.currency_rupee,
+                    text1: "Marketing",
+                    text2: "Designing",
+                    boxColor: Color(0xff63D461)),
+                CardStore(
+                    boxIcon: Icons.ac_unit,
+                    text1: "Marketing",
+                    text2: "Designing",
+                    boxColor: Color(0xffF6B152)),
+                CardStore(
+                    boxIcon: Icons.switch_account_rounded,
+                    text1: "Marketing",
+                    text2: "Designing",
+                    boxColor: Color(0xff13AD9E)),
+                CardStore(
+                    boxIcon: Icons.qr_code,
+                    text1: "Marketing",
+                    text2: "Designing",
+                    boxColor: Color(0xff7D7D7D)),
+                CardStore(
+                    boxIcon: Icons.bookmarks_sharp,
+                    text1: "Marketing",
+                    text2: "Designing",
+                    boxColor: Color(0xff6E41B1)),
+                CardStore(
+                  boxIcon: Icons.format_align_left,
+                  text1: "Marketing",
+                  text2: "Designing",
+                  boxColor: Color(0xffC15A85),
+                  newStatus: true,
+                ),
               ],
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 20,
                 mainAxisSpacing: 20,
--- lib/refactoring.dart
+++ lib/refactoring.dart
@@ -2,23 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AdditionalTiles extends StatelessWidget {
-
   final leadIcon;
   final text;
   final trailIcon;
 
-   AdditionalTiles({ Key? key, required this.leadIcon, required this.text, this.trailIcon }) : super(key: key);
+  AdditionalTiles(
+      {Key? key, required this.leadIcon, required this.text, this.trailIcon})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-              leading: Icon(leadIcon),
-              title: Text(
-                text,
-                style: const TextStyle(fontSize: 20),
-              ),
-              trailing: trailIcon,
-            );
+      leading: Icon(leadIcon),
+      title: Text(
+        text,
+        style: const TextStyle(fontSize: 20),
+      ),
+      trailing: trailIcon,
+    );
   }
 }
 
@@ -29,40 +30,78 @@ class CardStore extends StatelessWidget {
   final boxColor;
   final text1;
   final text2;
-  const CardStore({ Key? key,required this.boxIcon,required this.text1,required this.text2,required this.boxColor}) : super(key: key);
+  final newStatus;
+  final textStyles=const TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
+  CardStore(
+      {Key? key,
+      required this.boxIcon,
+      required this.text1,
+      required this.text2,
+      required this.boxColor,
+      this.newStatus = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 40,
+                  width: 40,
                   decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(20),
+                    color: boxColor,
+                    borderRadius: BorderRadius.circular(5),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            color: boxColor,
-                            borderRadius: BorderRadius.circular(5),
-                          ),
-                          child: Center(
-                            child: Icon(boxIcon,color: Colors.white,),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text(text1,style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
-                       Text(text2,style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),),
-                      ],
+                  child: Center(
+                    child: Icon(
+                      boxIcon,
+                      color: Colors.white,
                     ),
                   ),
-                );
+                ),
+                Visibility(
+                  visible: newStatus,
+                  child: Container(
+                      width: 40,
+                      height: 25,
+                      decoration: BoxDecoration(
+                          color: Color(0xff17B51C),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: const Center(
+                        child: Text(
+                          "New",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              text1,
+              style: textStyles,
+            ),
+            Text(
+              text2,
+              style: textStyles,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
