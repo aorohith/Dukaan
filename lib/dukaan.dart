a6f7b0fbd74cf5ea079f9b3d79a8014113b5de60
aorohith <aorohith@gmail.com>
2022-03-29 09:37:29 +0530

    finished

--- lib/dukaan.dart
+++ lib/dukaan.dart
@@ -6,15 +6,14 @@ import 'package:ui_designs/Dukaan_Premium/dukaan_premium.dart';
 import 'package:ui_designs/Dukaan_Accounts/dukan_account.dart';
 
 class Dukan extends StatefulWidget {
-  
-  const Dukan({ Key? key }) : super(key: key);
+  const Dukan({Key? key}) : super(key: key);
 
   @override
   State<Dukan> createState() => _DukanState();
 }
 
 class _DukanState extends State<Dukan> {
-   int _currentSelectIndex = 0;
+  int _currentSelectIndex = 0;
   final _pages = [
     PremiumPage(),
     PaymentScreen(),
@@ -27,20 +26,28 @@ class _DukanState extends State<Dukan> {
     return Scaffold(
       body: _pages[_currentSelectIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentSelectIndex ,
-        selectedItemColor:Colors.blue,
+        currentIndex: _currentSelectIndex,
+        selectedItemColor: Colors.blue,
         unselectedItemColor: Colors.grey,
-        onTap: (indx){
-          setState(() {
-            _currentSelectIndex = indx;
-          });
+        onTap: (indx) {
+          setState(
+            () {
+              _currentSelectIndex = indx;
+            },
+          );
         },
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home,), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.currency_rupee_sharp),label: "Orders"),
-          BottomNavigationBarItem(icon: Icon(Icons.dashboard_outlined),label: "Products"),
-          BottomNavigationBarItem(icon: Icon(Icons.bolt),label: "Manage"),
-          BottomNavigationBarItem(icon: Icon(Icons.person),label: "Accounts"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.currency_rupee_sharp), label: "Orders"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard_outlined), label: "Products"),
+          BottomNavigationBarItem(icon: Icon(Icons.bolt), label: "Manage"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Accounts"),
         ],
       ),
     );
